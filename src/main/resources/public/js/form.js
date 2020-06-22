ea6df15205e2bc334ebdaab2f6b4c362d1e72f48
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-06-22 16:00:24 -0400

    form js

---
+++ src/main/resources/public/js/form.js
@@ -0,0 +1,12 @@
+const settingFormSubmitButtonID = "settings-form-submit";
+const settingServiceInputClass = "settings-checkbox";
+document.getElementById(settingFormSubmitButtonID).onclick = () => {
+    const enableServices = [];
+    for (const serviceCheckbox of document.getElementsByClassName(settingServiceInputClass)) {
+
+        if (serviceCheckbox.checked) {
+            enableServices.push(serviceCheckbox.value);
+        }
+    }
+    console.log(enableServices);
+};
