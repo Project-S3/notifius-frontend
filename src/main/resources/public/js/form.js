6a6d877e7e74edd605a536940c25ae003d831ce8
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-06-22 16:02:33 -0400

    form js

--- src/main/resources/public/js/form.js
+++ src/main/resources/public/js/form.js
@@ -3,7 +3,6 @@ const settingServiceInputClass = "settings-checkbox";
 document.getElementById(settingFormSubmitButtonID).onclick = () => {
     const enableServices = [];
     for (const serviceCheckbox of document.getElementsByClassName(settingServiceInputClass)) {
-
         if (serviceCheckbox.checked) {
             enableServices.push(serviceCheckbox.value);
         }
