5137c1cfd306f3c23d31b4a7076c5829ff11af5a
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-06-22 16:08:45 -0400

    form js

--- src/main/resources/public/js/form.js
+++ src/main/resources/public/js/form.js
@@ -1,11 +1,24 @@
+const NOTIFIUS_BACKEND_URL = "";
+
 const settingFormSubmitButtonID = "settings-form-submit";
-const settingServiceInputClass = "settings-checkbox";
+const settingServiceInputClass = "settings-service-checkbox";
+const settingNotificationSenderInputClass = "settings-notification-sender-checkbox";
+
 document.getElementById(settingFormSubmitButtonID).onclick = () => {
     const enableServices = [];
+    const notificationSenders = {};
     for (const serviceCheckbox of document.getElementsByClassName(settingServiceInputClass)) {
         if (serviceCheckbox.checked) {
             enableServices.push(serviceCheckbox.value);
         }
     }
-    console.log(enableServices);
+    for (const notificationSenderInput of document.getElementsByClassName(settingNotificationSenderInputClass)) {
+        if (notificationSenderInput.value) {
+            notificationSenders[notificationSenderInput.name] = notificationSenderInput.value
+        }
+    }
+    const settings = { enableServices, notificationSenders }
+
+    // todo call ajax if works
+    console.log(settings);
 };
