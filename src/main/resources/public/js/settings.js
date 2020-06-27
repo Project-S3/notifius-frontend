ed229d7cc546b33c4a5b4fce734dac1815f77c14
Mathieu Gravel <mathieu.b.gravel@gmail.com>
2020-06-27 10:58:57 -0400

    warn in change

--- src/main/resources/public/js/settings.js
+++ src/main/resources/public/js/settings.js
@@ -3,6 +3,33 @@ const settingServiceInputClass = "settings-service-checkbox";
 const settingNotificationSenderValueInputClass = "settings-notification-sender-input";
 
 document.getElementById(settingFormSubmitButtonID).onclick = function () {
+    fetch("/settings", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(getSettingsFromForm())
+    }).then(response => {
+        if (response.ok) {
+            location.reload();
+        }
+    });
+};
+
+const testNotificationButtonID = "test-notification-button";
+const initialSetting = getSettingsFromForm();
+
+document.getElementById(testNotificationButtonID).onclick = function () {
+    if (JSON.stringify(initialSetting) !== JSON.stringify(getSettingsFromForm())) {
+        if (!window.confirm("Vous avez des changements non sauvegardés, voulez-vous continuer")) return;
+    }
+
+    fetch("/send-notification", {
+        method: "POST"
+    }).then(() => {
+        alert("Notification envoyée")
+    });
+};
+
+function getSettingsFromForm() {
     const enableServices = [];
     const notificationSenders = {};
     for (const serviceCheckbox of document.getElementsByClassName(settingServiceInputClass)) {
@@ -15,25 +42,5 @@ document.getElementById(settingFormSubmitButtonID).onclick = function () {
             notificationSenders[notificationSenderInput.id.replace("Value", "")] = notificationSenderInput.value
         }
     }
-    const settings = {enableServices, notificationSenders}
-
-    fetch("/settings", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(settings)
-    }).then(response => {
-        if (response.ok) {
-            location.reload();
-        }
-    });
-};
-
-const testNotificationButtonID = "test-notification-button";
-
-document.getElementById(testNotificationButtonID).onclick = function () {
-    fetch("/send-notification", {
-        method: "POST"
-    }).then(() => {
-        alert("Notification envoyée")
-    });
-};
+    return {enableServices, notificationSenders}
+}
